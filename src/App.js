cfa6700cffdf27260cd37613306e6179b69a3655
NilayYadav <nilayyadva10@gmail.com>
2022-12-10 13:33:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Imagecarousel from './Components/Imagecarousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Imagecarousel />
+    </div>
+  );
+}
+
+export default App;
